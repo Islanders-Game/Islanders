3acbef284f62574ad927f2a3d0381e36df383c1f
Anders Fischer-Nielsen <andersfischern@me.com>
2018-10-09 17:28:26 +0200

    Added separate Rule implementation.

---
+++ pilgrims-shared/src/Rules.ts
@@ -0,0 +1,28 @@
+import { Rules } from "./Shared";
+
+const rules: Rules = {
+    "Build House": (p, x, y) => (w) => {
+        return { tag: "Success", world: w} //TODO: Implement rule.
+    },
+    "Build City": (p, x, y) => (w) => {
+        return { tag: "Success", world: w} //TODO: Implement rule.
+    },
+    "Build Road": (p, x, y) => (w) => {
+        return { tag: "Success", world: w} //TODO: Implement rule.
+    },
+    "Move Thief": (p, x, y) => (w) => {
+        return { tag: "Success", world: w} //TODO: Implement rule.
+    },
+    "Buy Card": (p) => (w) => {
+        return { tag: "Success", world: w} //TODO: Implement rule.
+    },
+    "Play Card": (p, c) => (w) => {
+        return { tag: "Success", world: w} //TODO: Implement rule.
+    },
+    "Trade": (p1, p2) => (w) => {
+        return {tag: "Success", world: w} //TODO: Implement rule.
+    }
+}
+
+const partiallyAppliedBuild = rules["Build City"](2, 2, 2);
+// Call tryBuild() with a World, resulting in maybe a new World. 
