585c1a5ff94e265a0db788b56522ef708c207459
Anders Fischer-Nielsen <andersfischern@me.com>
2018-10-09 22:11:19 +0200

    Added tsconfig.json, made tsc shut up about types.

--- pilgrims-shared/lib/Rules.ts
+++ pilgrims-shared/lib/Rules.ts
@@ -2,15 +2,21 @@ import { Rules, World, Resources, MatrixCoordinate, House, Failure } from "./Sha
 
 // ---- Helpers ----
 const subtractResources = (resources: Resources, toSubtract: Resources) => {
-    return { clay: resources.clay - toSubtract.clay,
-             grain: resources.grain - toSubtract.grain,
-             stone: resources.stone - toSubtract.stone,
-             wood: resources.wood - toSubtract.wood,
-             wool: resources.wool - toSubtract.wool }
+    return {   
+        clay: resources.clay && toSubtract.clay ? resources.clay - toSubtract.clay : resources.clay,
+        grain: resources.grain && toSubtract.grain ? resources.grain - toSubtract.grain : resources.grain,
+        stone: resources.stone && toSubtract.stone ? resources.stone - toSubtract.stone : resources.stone,
+        wood: resources.wood && toSubtract.wood ? resources.wood - toSubtract.wood : resources.wood,
+        wool: resources.wool && toSubtract.wool ? resources.wool - toSubtract.wool : resources.wool
+    }
 }
 
 const resourcesAreNonNegative = (resources: Resources) => {
-    return Object.keys(resources).every(k => resources[k] !== undefined && Number(resources[k]) >= 0);
+    return resources.clay && resources.clay >= 0 &&
+        resources.grain && resources.grain >= 0 &&
+        resources.wood && resources.wood >= 0 &&
+        resources.wool && resources.wool >= 0 &&
+        resources.stone && resources.stone >= 0
 }
 
 const canPlaceHouse = (c: MatrixCoordinate, w: World) => {
