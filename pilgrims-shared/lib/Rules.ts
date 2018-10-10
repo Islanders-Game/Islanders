c63cc2376a8c2d2afbee008c98712b6adbf731cd
Anders Fischer-Nielsen <andersfischern@me.com>
2018-10-10 16:23:10 +0200

    Implemented buildRoad, made player fetching monadic, simplified things.

--- pilgrims-shared/lib/Rules.ts
+++ pilgrims-shared/lib/Rules.ts
@@ -1,4 +1,32 @@
-import { Rules, World, Resources, MatrixCoordinate, House, Failure, Success, Result } from './Shared';
+import { Rules, World, Resources, MatrixCoordinate, House, City, Road, Player, Failure,
+    Success, Result } from './Shared';
+
+// ---- Rule implementations ----
+const rules: Rules = {
+    'Build House': (id, coordinates) => (w) => {
+        if (w.tag === 'Failure') { return w; }
+        const player = findPlayer(id)(w);
+        const purchased = purchase(new House().cost)(player)(w);
+        const placed = placeHouse(coordinates)(player)(purchased);
+        return placed;
+    },
+    'Build City': (id, coordinates) => (w) => {
+        if (w.tag === 'Failure') { return w; }
+        const player = findPlayer(id)(w);
+        const purchased = purchase(new City().cost)(player)(w);
+        return placeCity(coordinates)(player)(purchased);
+    },
+    'Build Road': (id, start, end) => (w) => {
+        if (w.tag === 'Failure') { return w; }
+        const player = findPlayer(id)(w);
+        const purchased = purchase(new Road().cost)(player)(w);
+        return placeRoad(start, end)(player)(purchased);
+    },
+    'Move Thief': (p, {x, y}) => (w) => w, // TODO: Implement rules.
+    'Buy Card': (p) => (w) => w,
+    'Play Card': (p, c) => (w) => w,
+    'Trade': (p1, p2) => (w) => w,
+};
 
 // ---- Helpers ----
 export const subtractResources = (resources: Resources, toSubtract: Resources) => {
@@ -19,86 +47,62 @@ const resourcesAreNonNegative = (resources: Resources) => {
         resources.stone && resources.stone >= 0;
 };
 
-const canPlaceHouse = (coord: MatrixCoordinate, w: World) => {
-    const all = w.players.reduce((acc: House[], p) => acc.concat(p.houses), []);
-    return all.every((h) =>
+function fail(reason: string): Failure { return { tag: 'Failure', reason }; }
+function success<T>(t: T): Success<T> { return { tag: 'Success', world: t }; }
+
+const purchase = (cost: Resources) => (p: Result<Player>) => (r: Result<World>) => {
+    if (r.tag === 'Failure') { return r; }
+    if (p.tag === 'Failure') { return p; }
+    const resources = subtractResources(p.world.resources, cost);
+    if (!resourcesAreNonNegative(resources)) { return fail(`Player ${p.world.id} cannot afford this.`); }
+    const players = r.world.players.map((pl) => (pl.id === p.world.id) ? {...p.world, resources} : pl);
+    return success({ ...r.world, players });
+};
+
+const findPlayer = (id: number) => (r: Result<World>) => {
+    if (r.tag === 'Failure') { return r; }
+    const player = r.world.players.find((pl) => pl.id === id);
+    if (!player) { return fail(`Player ${id} not found.`); }
+    return success(player);
+};
+
+const placeHouse = (coord: MatrixCoordinate) => (p: Result<Player>) => (r: Result<World>) => {
+    if (r.tag === 'Failure') { return r; }
+    if (p.tag === 'Failure') { return p; }
+    const allHouses = r.world.players.reduce((acc: House[], p) => acc.concat(p.houses), []);
+    const canPlace = allHouses.every((h) =>
         (h.position.x !== coord.x && h.position.y !== coord.y)
         && (h.position.y !== coord.y - 1 && h.position.x !== coord.x - 1)  // Adjecent matrix corners.
         && (h.position.y !== coord.y - 1 && h.position.x !== coord.x + 1)  // Adjecent matrix corners.
         && (h.position.y !== coord.y + 1 && h.position.x !== coord.x - 1)  // Adjecent matrix corners.
-        && (h.position.y !== coord.y + 1 && h.position.x !== coord.x + 1)); // Adjecent matrix corners.
+        && (h.position.y !== coord.y + 1 && h.position.x !== coord.x + 1));
+    if (!canPlace) { return fail('Cannot place a house here!'); }
+    const addedHouse = p.world.houses.concat([new House(coord)]);
+    const players = r.world.players.map((pl) => (pl.id === p.world.id) ? {...p.world, houses: addedHouse} : pl);
+    return success( {...r.world, players });
 };
 
-function fail(reason: string): Failure { return { tag: 'Failure', reason }; }
-function success<T>(t: T): Success<T> { return { tag: 'Success', world: t }; }
-
-type validationFunction = (x: Result<World>) => (Result<World>);
-const validationReducer = (acc: Result<World>, curr: validationFunction) => curr(acc);
-
-// Validation functions can take more parameters - just end up as a validation function
-const purchaseHouse: (t: Resources) => validationFunction = (w: Resources) => (t: Result<World>) => {
-    if (t.tag === 'Success') {
-        // make check
-        return success(t.world);
-    } else {
-        return t;
-    }
-};
-const placeHouse: (t: World) => validationFunction = (w: World) => (t: Result<World>) => {
-    if (t.tag === 'Success') {
-        // make check
-        return success(t.world);
-    } else {
-        return t;
-    }
+const placeCity = (coord: MatrixCoordinate) => (p: Result<Player>) => (r: Result<World>) => {
+    if (r.tag === 'Failure') { return r; }
+    if (p.tag === 'Failure') { return p; }
+    const canPlace = p.world.houses.some((h) => h.position.x === coord.x && h.position.y === coord.y);
+    if (!canPlace) { return fail('Cannot place a house here!'); }
+    const houses = p.world.houses.filter((h) => h.position !== coord);
+    const cities = p.world.cities.concat([new City(coord)]);
+    const players = r.world.players.map((pl) => (pl.id === p.world.id) ? {...p.world, houses, cities} : pl);
+    return success( {...r.world, players });
 };
 
-// ---- Rule implementations ----
-const rules: Rules = {
-    'Build House': (p, x, y) => (w) => {
-        switch (w.tag) {
-            case 'Success': {
-                const player = w.world.players.find((pl) => p === pl.id);
-                if (!player) { return fail('The player could not be found!'); }
-
-                if (!canPlaceHouse({x, y}, w.world)) { return fail('The house cannot be placed here!'); }
-
-                const modifiedResources = subtractResources(player.resources, new House().cost);
-                const canAfford = resourcesAreNonNegative(modifiedResources);
-                if (!canAfford) { return fail('The player cannot afford this!'); }
-
-                const modifiedPlayer = {...player, resources: modifiedResources};
-                const modifiedPlayers = w.world.players.map((pl) => (pl.id === p) ? modifiedPlayer : pl);
-                const world = {...w.world, players: modifiedPlayers};
-
-                return success(world);
-            }
-            case 'Failure': {
-                return w;
-            }
-        }
-    },
-    'Build City': (p, x, y) => (w) => {
-        switch (w.tag) {
-            case 'Success': {
-                // Validate that the rule is legal
-                const effectFunctions = [
-                    purchaseHouse(w.world.players[0].resources),
-                    placeHouse(w.world)];
-                const result = effectFunctions.reduce(validationReducer, success(w.world));
-                return result;
-            }
-            case 'Failure': {
-                return w;
-            }
-        }
-    },
-    'Build Road': (p, x, y) => (w) => w, // TODO: Implement rules.
-    'Move Thief': (p, x, y) => (w) => w,
-    'Buy Card': (p) => (w) => w,
-    'Play Card': (p, c) => (w) => w,
-    'Trade': (p1, p2) => (w) => w,
+const placeRoad = (start: MatrixCoordinate, end: MatrixCoordinate) => (p: Result<Player>) => (r: Result<World>) => {
+    if (r.tag === 'Failure') { return r; }
+    if (p.tag === 'Failure') { return p; }
+    const canPlace = p.world.roads.some((ro) => ro.start !== start && ro.end !== end);
+    if (!canPlace) { return fail('Cannot place a road here!'); }
+    const roads = p.world.roads.concat([new Road(start, end)]);
+    const players = r.world.players.map((pl) => (pl.id === p.world.id) ? {...p.world, roads} : pl);
+    return success( {...r.world, players });
 };
+
 
 // -----------------------
 // Example usage of above:
@@ -115,8 +119,8 @@ const initialWorld: World = {
     map: [{type: 'Wool', diceRoll: 12}],
 };
 
-const tryBuildHouse = rules['Build House'](2, 2, 2);
-const tryBuildCity = rules['Build City'](2, 2, 2);
+const tryBuildHouse = rules['Build House'](2, {x: 2, y: 2});
+const tryBuildCity = rules['Build City'](2, {x: 2, y: 2});
 const toApply = [tryBuildHouse, tryBuildCity];
 
 const ruleReducer = (acc: Result<World>, curr: ((x: Result<World>) => (Result<World>))) => curr(acc);
