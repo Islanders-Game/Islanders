713158473c525506596b0b2ee582162886de8b2a
Anders Wind <awia00@gmail.com>
2018-10-10 13:00:15 +0200

    Added tabs to the overview

--- pilgrims-client/src/components/Overview.vue
+++ pilgrims-client/src/components/Overview.vue
@@ -1,8 +1,25 @@
 <template>
     <div id="Overview">
-      <h1 style="color: white">
-        Overview
-      </h1>
+      <v-tabs centered color="cyan" dark icons-and-text>
+        <v-tabs-slider color="yellow"></v-tabs-slider>
+        <v-tab href="#tab-1">
+          Players
+          <v-icon>phone</v-icon>
+        </v-tab>
+        <v-tab href="#tab-2">
+          Chat
+          <v-icon>favorite</v-icon>
+        </v-tab>
+        <v-tab href="#tab-3">
+          Log
+          <v-icon>account_box</v-icon>
+        </v-tab>
+        <v-tab-item v-for="i in 3" :id="'tab-' + i" :key="i">
+          <v-card flat >
+            <v-card-text class="dark-card">{{ text }}</v-card-text>
+          </v-card>
+        </v-tab-item>
+      </v-tabs>
     </div>
 </template>
 
@@ -12,7 +29,7 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 @Component
 export default class Overview extends Vue {
   @Prop()
-  private msg!: string;
+  private text: string = 'make a component here';
 }
 </script>
 
@@ -23,5 +40,9 @@ export default class Overview extends Vue {
   align-self: auto;
   background-color: #25262a;
   margin-right: 0;
+}
+.dark-card {
+  background-color: rgba(0,0,0,.87);
+  color: whitesmoke;
 }
 </style>
