53de4d543f2f5ef482aee1a3a5a7368056f09407
Anders Fischer-Nielsen <andersfischern@me.com>
2018-10-11 19:49:40 +0200

    Used viewport for adding graphics enabling pan/zoom.

--- pilgrims-client/src/components/Map.vue
+++ pilgrims-client/src/components/Map.vue
@@ -6,8 +6,9 @@
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
 import { defineGrid, extendHex } from 'honeycomb-grid';
-import { Graphics, Sprite, Application, Point } from 'pixi.js';
-import * as svgPanZoom from 'svg-pan-zoom';
+import { Graphics, Sprite, Application, Point, Texture } from 'pixi.js';
+import Viewport from 'pixi-viewport';
+
 
 @Component
 export default class Map extends Vue {
@@ -28,31 +29,56 @@ export default class Map extends Vue {
 
   constructor() {
     super();
+  }
 
-    window.addEventListener('load', (event) => {
-      this.DrawMap();
-    });
+  private mounted() {
+    this.DrawMap();
+  }
+
+  private gradient(from, to): Texture {
+    const c = document.createElement("canvas");
+    const ctx = c.getContext("2d");
+    const grd = ctx.createLinearGradient(0,0,100,100);
+    grd.addColorStop(0, from);
+    grd.addColorStop(1, to);
+    ctx.fillStyle = grd;
+    ctx.fillRect(0,0,100,100);
+    return Texture.from(c);
   }
 
   private DrawMap(): void {
-    const app = new Application({transparent: true});
-    const graphics = new Graphics();
-    graphics.lineStyle(1, 0x999999);
-    document.getElementById('Map')!.appendChild(app.view);
-
     const Hex = extendHex({
       size: 200,
       orientation: 'flat',
     });
     const Grid = defineGrid(Hex);
-    Grid.rectangle({ width: 12, height: 10 }).forEach((hex) => {
+
+    const app = new Application({transparent: true, height: this.$el.clientHeight, width: this.$el.clientWidth});
+    const graphics = new Graphics();
+    const viewport = new Viewport({
+      screenWidth: window.innerWidth,
+      screenHeight: window.innerHeight,
+      worldWidth: 100,
+      worldHeight: 100,
+      interaction: app.renderer!.plugins.interaction,
+    });
+
+    graphics.lineStyle(10, 0xFFFFFF);
+    app.stage.addChild(viewport);
+    viewport.drag().pinch().wheel().decelerate();
+    this.$el.appendChild(app.view);
+
+    Grid.hexagon({ radius: 20 }).forEach((hex) => {
       const point = hex.toPoint();
       const corners = hex.corners().map((corner) => corner.add(point));
       const [firstCorner, ...otherCorners] = corners;
+
+      graphics.beginFill(Math.random());
       graphics.moveTo(firstCorner.x, firstCorner.y);
       otherCorners.forEach(({ x, y }) => graphics.lineTo(x, y));
       graphics.lineTo(firstCorner.x, firstCorner.y);
-      app.stage.addChild(graphics);
+      graphics.endFill();
+      viewport.addChild(graphics);
     });
   }
 }
