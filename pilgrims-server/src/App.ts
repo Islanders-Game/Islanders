389bf61ff3af931103d426a4d4b6e44b80585ee1
Anders Fischer-Nielsen <andersfischern@me.com>
2018-10-12 14:59:21 +0200

    First attempt at implementing application of rules on server. UNTESTED.

--- pilgrims-server/src/App.ts
+++ pilgrims-server/src/App.ts
@@ -3,6 +3,8 @@ import * as socket from 'socket.io';
 import * as rethink from 'rethinkdb';
 import * as bodyParser from 'body-parser';
 import * as http from 'http';
+import { Result, Rule, Turn, World, Action, Player, ChatMessage, Rules } from 'pilgrims-shared';
+import { rules, ruleReducer } from 'pilgrims-shared/lib/Rules';
 
 const app = express();
 const server = new http.Server(app);
@@ -39,21 +41,20 @@ io.on('connection', (socket) => {
         console.info(`'game_start' with game ${game}.`);
         io.of(`/${game.id}`)
           .on('join', (message) => {
-                const player /*: Player*/ = JSON.parse(message);
+                const player: Player = JSON.parse(message);
                 if (!player || !player.id) return;
                 console.info(`'join' on game ${game.id} by ${player.id}.`);
                 const result = addPlayer(game.id, player);
                 socket.to(game.id).emit('joined', result);
             })
           .on('turn_end', (message) => {
-                const turn /*: Turn*/= JSON.parse(message);
+                const turn: Turn = JSON.parse(message);
                 if (!turn || !turn.player || !turn.actions) return;
                 console.info(`'turn_end' on game ${game.id} with ${turn}.`);
-                const applied /*: Result<World>*/ = applyTurn(game.id, turn);
-                socket.to(game.id).emit('apply_turn', applied);
+                applyTurn(game.id, turn).then((res) => socket.to(game.id).emit('apply_turn', res));
           })
           .on('chat', (message) => {
-                const chatMessage /*: ChatMessage*/ = JSON.parse(message);
+                const chatMessage: ChatMessage = JSON.parse(message);
                 if (!chatMessage || !chatMessage.user || !chatMessage.text) return;
                 console.info(`'chat' on game ${game.id} by ${chatMessage.user} with text ${chatMessage.text}.`);
                 socket.to(game.id).emit('chat', message);
@@ -66,30 +67,48 @@ io.on('connection', (socket) => {
 
 
 //Game
-const applyTurn = (id: string, turn /*: Turn*/)/*: Result<World>*/ => {
-    const toApply = mapRules(turn.actions) 
-    const result = findWorld(id);
-    //TODO: Apply all rules to world using Shared application code. Return Result.
-    return "";
+const applyTurn = async (id: string, turn: Turn) => {
+    const toApply = mapRules(turn.actions)
+    const world = await findWorld(id);
+    const result = toApply.reduce(ruleReducer, world);
+    return result;
 }
 
-const mapRules = (actions /*: Action[]*/) => {
-    //TODO: Lookup actions in Shared.Rules. 
-    //TODO: Return mapped rules as array | undefined or Result(?).
-    return [];
+const mapRules = (actions : Action[]) => {
+    const mapped: Rule[] = actions.map((a) => {
+        if (a.buildCity)    return rules["Build City"](a.buildCity.playerID, a.buildCity.coordinates);
+        if (a.buildHouse)   return rules["Build House"](a.buildHouse.playerID, a.buildHouse.coordinates);
+        if (a.buildRoad)    return rules["Build Road"](a.buildRoad.playerID, a.buildRoad.start, a.buildRoad.end);
+        if (a.buyCard)      return rules["Buy Card"](a.buyCard.playerID);
+        if (a.moveThief)    return rules["Move Thief"](a.moveThief.playerID, a.moveThief.coordinates);
+        if (a.playCard)     return rules["Play Card"](a.playCard.playerID, a.playCard.card);
+        if (a.trade)        return rules["Trade"](a.trade.playerID, a.trade.otherPlayerID, a.trade.resources);
+        return undefined
+    });
+    if (mapped.some(r => r === undefined)) return undefined;
+    return mapped;
 }
 
-const addPlayer = async (id: string, player /*: Player*/)/*: Result<World>*/ => {
-    const result = await findWorld(id);
-    if (result.tag === 'Failure') return result;
-    const players = result.world //TODO: .players.concat(newPlayer);
-    return { tag: 'Success', world: { ...result.world, players }};
+const addPlayer = async (id: string, player: Player) => {
+    try {
+        const result: Result<World> = await findWorld(id);
+        if (result.tag === 'Failure') return result;
+        const players = result.world.players.concat(player);
+        return { tag: 'Success', world: { ...result, players }};
+    } catch {
+        return { tag: 'Failure'}
+    }
 }
 
-const findWorld = async (uuid: string)/*: Result<World>*/ => {
-    const world = await rethink.table('games').get(uuid).run(connection);
-    if (world) return { tag: 'Success', world };
-    return { tag: 'Failure', error: 'World not found!' };
+async function findWorld(id: string): Promise<Result<World>> {
+    try {
+        const dbResult = await rethink.table('games').get(id).run(connection);
+        const result = dbResult as World;
+        if (!(result as World)) return { tag: 'Failure', reason: "World could not be found!" };
+        return { tag: 'Success', world: result };
+    } catch {
+        return { tag: 'Failure', reason: "World could not be found!" };
+    }
 }
 //
 
