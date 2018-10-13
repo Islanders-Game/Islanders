e7bac122ef9f8e134a13e340fb13417f14a8ed42
Anders Wind <awia00@gmail.com>
2018-10-13 15:45:43 +0200

    Changed rule methods from strings to function

--- pilgrims-server/src/App.ts
+++ pilgrims-server/src/App.ts
@@ -4,115 +4,166 @@ import http = require('http');
 
 import * as bodyParser from 'body-parser';
 import * as socket from 'socket.io';
-import { Result, Rule, Turn, World, Action, Player, ChatMessage, rules, ruleReducer } from 'pilgrims-shared';
+import {
+  Result,
+  Rule,
+  Turn,
+  World,
+  Action,
+  Player,
+  ChatMessage,
+  rules,
+  ruleReducer,
+} from 'pilgrims-shared';
 
 const app = express();
 const server = new http.Server(app);
 const io = socket(server);
 const port = 3000;
-let connection: rethink.Connection | undefined;
+let connection: rethink.Connection | undefined;
 
 // RethinkDB
-rethink.connect( {host: 'localhost', port: 28015}, (err, c) => {
-    if (err) { throw err; }
+rethink.connect(
+  { host: 'localhost', port: 28015 },
+  (err, c) => {
+    if (err) {
+      throw err;
+    }
     connection = c;
-});
+  },
+);
 
-rethink.db('pilgrims').tableCreate('games').run(connection, (err, result) => {
-    if (err) { throw err; }
+rethink
+  .db('pilgrims')
+  .tableCreate('games')
+  .run(connection, (err, result) => {
+    if (err) {
+      throw err;
+    }
     console.log(JSON.stringify(result, null, 2));
-});
+  });
 
 app.post('/newGame', (req, res) => {
-    const world = req.body;
-    rethink.table('games').insert(world).run(connection, (err, result) => {
-        if (err) { throw err; }
-        res.send(result. generated_keys[0]);
+  const world = req.body;
+  rethink
+    .table('games')
+    .insert(world)
+    .run(connection, (err, result) => {
+      if (err) {
+        throw err;
+      }
+      res.send(result.generated_keys[0]);
     });
 });
 //
 
 //Socket.io
 io.on('connection', (socket) => {
-    console.info(`Player connected on ${socket.id}.`)
-    socket.on('game_start', (message) => {
-        const game = JSON.parse(message);
-        if (!game || !game.id) return;
-        console.info(`'game_start' with game ${game}.`);
-        io.of(`/${game.id}`)
-          .on('join', (message) => {
-                const player: Player = JSON.parse(message);
-                if (!player || !player.id) return;
-                console.info(`'join' on game ${game.id} by ${player.id}.`);
-                const result = addPlayer(game.id, player);
-                socket.to(game.id).emit('joined', result);
-            })
-          .on('turn_end', (message) => {
-                const turn: Turn = JSON.parse(message);
-                if (!turn || !turn.player || !turn.actions) return;
-                console.info(`'turn_end' on game ${game.id} with ${turn}.`);
-                applyTurn(game.id, turn).then((res) => socket.to(game.id).emit('apply_turn', res));
-          })
-          .on('chat', (message) => {
-                const chatMessage: ChatMessage = JSON.parse(message);
-                if (!chatMessage || !chatMessage.user || !chatMessage.text) return;
-                console.info(`'chat' on game ${game.id} by ${chatMessage.user} with text ${chatMessage.text}.`);
-                socket.to(game.id).emit('chat', message);
-          });
-        });
-
-    socket.on('disconnect', (socket) => console.info(`Socket ${socket.id} disconnected.`));
+  console.info(`Player connected on ${socket.id}.`);
+  socket.on('game_start', (message) => {
+    const game = JSON.parse(message);
+    if (!game || !game.id) return;
+    console.info(`'game_start' with game ${game}.`);
+    io.of(`/${game.id}`)
+      .on('join', (message) => {
+        const player: Player = JSON.parse(message);
+        if (!player || !player.id) return;
+        console.info(`'join' on game ${game.id} by ${player.id}.`);
+        const result = addPlayer(game.id, player);
+        socket.to(game.id).emit('joined', result);
+      })
+      .on('turn_end', (message) => {
+        const turn: Turn = JSON.parse(message);
+        if (!turn || !turn.player || !turn.actions) return;
+        console.info(`'turn_end' on game ${game.id} with ${turn}.`);
+        applyTurn(game.id, turn).then((res) =>
+          socket.to(game.id).emit('apply_turn', res),
+        );
+      })
+      .on('chat', (message) => {
+        const chatMessage: ChatMessage = JSON.parse(message);
+        if (!chatMessage || !chatMessage.user || !chatMessage.text) return;
+        console.info(
+          `'chat' on game ${game.id} by ${chatMessage.user} with text ${
+            chatMessage.text
+          }.`,
+        );
+        socket.to(game.id).emit('chat', message);
+      });
   });
-//
 
+  socket.on('disconnect', (socket) =>
+    console.info(`Socket ${socket.id} disconnected.`),
+  );
+});
+//
 
 //Game
 const applyTurn = async (id: string, turn: Turn) => {
-    const toApply = mapRules(turn.actions)
-    const world = await findWorld(id);
-    const result = toApply.reduce(ruleReducer, world);
-    return result;
-}
+  const toApply = mapRules(turn.actions);
+  const world = await findWorld(id);
+  const result = toApply.reduce(ruleReducer, world);
+  return result;
+};
 
-const mapRules = (actions : Action[]) => {
-    const mapped: Rule[] = actions.map((a) => {
-        if (a.buildCity)    return rules["Build City"](a.buildCity.playerID, a.buildCity.coordinates);
-        if (a.buildHouse)   return rules["Build House"](a.buildHouse.playerID, a.buildHouse.coordinates);
-        if (a.buildRoad)    return rules["Build Road"](a.buildRoad.playerID, a.buildRoad.start, a.buildRoad.end);
-        if (a.buyCard)      return rules["Buy Card"](a.buyCard.playerID);
-        if (a.moveThief)    return rules["Move Thief"](a.moveThief.playerID, a.moveThief.coordinates);
-        if (a.playCard)     return rules["Play Card"](a.playCard.playerID, a.playCard.card);
-        if (a.trade)        return rules["Trade"](a.trade.playerID, a.trade.otherPlayerID, a.trade.resources);
-        return undefined
-    });
-    if (mapped.some(r => r === undefined)) return undefined;
-    return mapped;
-}
+const mapRules = (actions: Action[]) => {
+  const mapped: Rule[] = actions.map((a) => {
+    if (a.buildCity)
+      return rules.BuildCity(a.buildCity.playerID, a.buildCity.coordinates);
+    if (a.buildHouse)
+      return rules.BuildHouse(a.buildHouse.playerID, a.buildHouse.coordinates);
+    if (a.buildRoad)
+      return rules.BuildRoad(
+        a.buildRoad.playerID,
+        a.buildRoad.start,
+        a.buildRoad.end,
+      );
+    if (a.buyCard) return rules.BuyCard(a.buyCard.playerID);
+    if (a.moveThief)
+      return rules.MoveThief(a.moveThief.playerID, a.moveThief.coordinates);
+    if (a.playCard) return rules.PlayCard(a.playCard.playerID, a.playCard.card);
+    if (a.trade)
+      return rules.Trade(
+        a.trade.playerID,
+        a.trade.otherPlayerID,
+        a.trade.resources,
+      );
+    return undefined;
+  });
+  if (mapped.some((r) => r === undefined)) return undefined;
+  return mapped;
+};
 
 const addPlayer = async (id: string, player: Player) => {
-    try {
-        const result: Result<World> = await findWorld(id);
-        if (result.tag === 'Failure') return result;
-        const players = result.world.players.concat(player);
-        return { tag: 'Success', world: { ...result, players }};
-    } catch {
-        return { tag: 'Failure'}
-    }
-}
+  try {
+    const result: Result<World> = await findWorld(id);
+    if (result.tag === 'Failure') return result;
+    const players = result.world.players.concat(player);
+    return { tag: 'Success', world: { ...result, players } };
+  } catch {
+    return { tag: 'Failure' };
+  }
+};
 
 async function findWorld(id: string): Promise<Result<World>> {
-    try {
-        const dbResult = await rethink.table('games').get(id).run(connection);
-        const result = dbResult as World;
-        if (!(result as World)) return { tag: 'Failure', reason: "World could not be found!" };
-        return { tag: 'Success', world: result };
-    } catch {
-        return { tag: 'Failure', reason: "World could not be found!" };
-    }
+  try {
+    const dbResult = await rethink
+      .table('games')
+      .get(id)
+      .run(connection);
+    const result = dbResult as World;
+    if (!(result as World))
+      return { tag: 'Failure', reason: 'World could not be found!' };
+    return { tag: 'Success', world: result };
+  } catch {
+    return { tag: 'Failure', reason: 'World could not be found!' };
+  }
 }
 //
 
 //Initialize
 app.use(bodyParser.json());
-server.listen(port, () => console.log(`pilgrims-server listening on port ${port}!`))
+server.listen(port, () =>
+  console.log(`pilgrims-server listening on port ${port}!`),
+);
 //
--- pilgrims-shared/lib/Shared.ts
+++ pilgrims-shared/lib/Shared.ts
@@ -1,241 +1,367 @@
 export interface Turn {
-    player: number;
-    actions: Action[];
+  player: number;
+  actions: Action[];
 }
 
 export interface Action {
-    buildHouse: undefined | { playerID: number, coordinates: MatrixCoordinate };
-    buildCity:  undefined | { playerID: number, coordinates: MatrixCoordinate };
-    buildRoad:  undefined | { playerID: number, start: MatrixCoordinate, end: MatrixCoordinate };
-    moveThief:  undefined | { playerID: number, coordinates: HexCoordinate };
-    buyCard:    undefined | { playerID: number };
-    playCard:   undefined | { playerID: number, card: DevelopmentCard };
-    trade:      undefined | { playerID: number, otherPlayerID: number, resources: Resources };
+  buildHouse: undefined | { playerID: number; coordinates: MatrixCoordinate };
+  buildCity: undefined | { playerID: number; coordinates: MatrixCoordinate };
+  buildRoad:
+    | undefined
+    | { playerID: number; start: MatrixCoordinate; end: MatrixCoordinate };
+  moveThief: undefined | { playerID: number; coordinates: HexCoordinate };
+  buyCard: undefined | { playerID: number };
+  playCard: undefined | { playerID: number; card: DevelopmentCard };
+  trade:
+    | undefined
+    | { playerID: number; otherPlayerID: number; resources: Resources };
 }
 
 export interface World {
-    players: Player[];
-    thief?: Thief;
-    map: Tile[];
+  players: Player[];
+  thief?: Thief;
+  map: Tile[];
 }
 
 export interface Thief {
-    hexCoordinate: HexCoordinate;
+  hexCoordinate: HexCoordinate;
 }
 
 export interface Player {
-    id: number;
-    resources: Resources;
-    houses: House[];
-    cities: City[];
-    roads: Road[];
-    ships: Ship[];
+  id: number;
+  resources: Resources;
+  houses: House[];
+  cities: City[];
+  roads: Road[];
+  ships: Ship[];
 }
 
-interface Purchaseable { cost: Resources; }
+interface Purchaseable {
+  cost: Resources;
+}
 export class House implements Purchaseable {
-    public position: MatrixCoordinate;
-    public cost = {
-        wood: 1,
-        clay: 1,
-        wool: 1,
-        grain: 1,
-    };
+  public position: MatrixCoordinate;
+  public cost = {
+    wood: 1,
+    clay: 1,
+    wool: 1,
+    grain: 1,
+  };
 
-    constructor(coordinates: MatrixCoordinate = {x: 0, y: 0}) {
-        this.position = coordinates;
-    }
+  constructor(coordinates: MatrixCoordinate = { x: 0, y: 0 }) {
+    this.position = coordinates;
+  }
 }
 
 export class City extends House {
-    public cost = {
-        grain: 2,
-        stone: 3,
-        wool: 0,
-        wood: 0,
-        clay: 0
-    };
+  public cost = {
+    grain: 2,
+    stone: 3,
+    wool: 0,
+    wood: 0,
+    clay: 0,
+  };
 }
 
 export class Road implements Purchaseable {
-    public start: MatrixCoordinate;
-    public end: MatrixCoordinate;
-    public cost = {
-        wood: 1,
-        clay: 1,
-    };
+  public start: MatrixCoordinate;
+  public end: MatrixCoordinate;
+  public cost = {
+    wood: 1,
+    clay: 1,
+  };
 
-    constructor(start: MatrixCoordinate = {x: 0, y: 0}, end: MatrixCoordinate = {x: 0, y: 0}) {
-        this.start = start;
-        this.end = end;
-    }
+  constructor(
+    start: MatrixCoordinate = { x: 0, y: 0 },
+    end: MatrixCoordinate = { x: 0, y: 0 },
+  ) {
+    this.start = start;
+    this.end = end;
+  }
 }
 
 export class Ship extends Road {
-    public cost = {
-        wood: 1,
-        wool: 1,
-        clay: 0
-    };
+  public cost = {
+    wood: 1,
+    wool: 1,
+    clay: 0,
+  };
 }
 
 export class DevelopmentCard implements Purchaseable {
-    public type: 'Knight' | 'Victory Point' | 'Road Building' | 'Monopoly' | 'Year of Plenty' | 'None' = 'None';
-    public cost = {
-        grain: 1,
-        stone: 1,
-        wool: 1,
-    };
+  public type:
+    | 'Knight'
+    | 'Victory Point'
+    | 'Road Building'
+    | 'Monopoly'
+    | 'Year of Plenty'
+    | 'None' = 'None';
+  public cost = {
+    grain: 1,
+    stone: 1,
+    wool: 1,
+  };
 }
 
 export interface Tile {
-    type: 'Wood' | 'Stone' | 'Clay' | 'Grain' | 'Wool' | 'Desert' | 'Ocean';
-    diceRoll: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
+  type: 'Wood' | 'Stone' | 'Clay' | 'Grain' | 'Wool' | 'Desert' | 'Ocean';
+  diceRoll: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
 }
 
 export interface HexCoordinate {
-    x: number;
-    y: number;
+  x: number;
+  y: number;
 }
 export interface MatrixCoordinate extends HexCoordinate {}
 
 export interface Resources {
-    wood?: number;
-    stone?: number;
-    clay?: number;
-    grain?: number;
-    wool?: number;
+  wood?: number;
+  stone?: number;
+  clay?: number;
+  grain?: number;
+  wool?: number;
 }
 
 export interface ChatMessage {
-    text: string;
-    user: string;
+  text: string;
+  user: string;
 }
 
 export interface Failure {
-    tag: 'Failure';
-    reason: string;
+  tag: 'Failure';
+  reason: string;
 }
 export interface Success<T> {
-    tag: 'Success';
-    world: T;
+  tag: 'Success';
+  world: T;
 }
-export type Result<T> = Success<T> | Failure;
+export type Result<T> = Success<T> | Failure;
 
 export type Rule = (w: Result<World>) => Result<World>;
 export interface Rules {
-    'Build House':  (playerID: number, coordinates: MatrixCoordinate) => (w: Result<World>) => Result<World>;
-    'Build City':   (playerID: number, coordinates: MatrixCoordinate) => (w: Result<World>) => Result<World>;
-    'Build Road':   (playerID: number, start: MatrixCoordinate, end: MatrixCoordinate)
-        => (w: Result<World>) => Result<World>;
-    'Move Thief':   (playerID: number, coordinates: HexCoordinate) => (w: Result<World>) => Result<World>;
-    'Buy Card':     (playerID: number) => (w: Result<World>) => Result<World>;
-    'Play Card':    (playerID: number, card: DevelopmentCard) => (w: Result<World>) => Result<World>;
-    'Trade':        (playerID: number, otherPlayerID: number, resources: Resources) => (w: Result<World>) => Result<World>;
+  BuildHouse: (
+    playerID: number,
+    coordinates: MatrixCoordinate,
+  ) => (w: Result<World>) => Result<World>;
+  BuildCity: (
+    playerID: number,
+    coordinates: MatrixCoordinate,
+  ) => (w: Result<World>) => Result<World>;
+  BuildRoad: (
+    playerID: number,
+    start: MatrixCoordinate,
+    end: MatrixCoordinate,
+  ) => (w: Result<World>) => Result<World>;
+  MoveThief: (
+    playerID: number,
+    coordinates: HexCoordinate,
+  ) => (w: Result<World>) => Result<World>;
+  BuyCard: (playerID: number) => (w: Result<World>) => Result<World>;
+  PlayCard: (
+    playerID: number,
+    card: DevelopmentCard,
+  ) => (w: Result<World>) => Result<World>;
+  Trade: (
+    playerID: number,
+    otherPlayerID: number,
+    resources: Resources,
+  ) => (w: Result<World>) => Result<World>;
 }
 
-export const ruleReducer = (acc: Result<World>, curr: ((x: Result<World>) => (Result<World>))) => curr(acc);
+export const ruleReducer = (
+  acc: Result<World>,
+  curr: ((x: Result<World>) => Result<World>),
+) => curr(acc);
 
 //
 // ---- Rule implementations ----
 //
 export const rules: Rules = {
-    'Build House': (id, coordinates) => (w) => {
-        if (w.tag === 'Failure') { return w; }
-        const player = findPlayer(id)(w);
-        const purchased = purchase(new House().cost)(player)(w);
-        const placed = placeHouse(coordinates)(player)(purchased);
-        return placed;
-    },
-    'Build City': (id, coordinates) => (w) => {
-        if (w.tag === 'Failure') { return w; }
-        const player = findPlayer(id)(w);
-        const purchased = purchase(new City().cost)(player)(w);
-        return placeCity(coordinates)(player)(purchased);
-    },
-    'Build Road': (id, start, end) => (w) => {
-        if (w.tag === 'Failure') { return w; }
-        const player = findPlayer(id)(w);
-        const purchased = purchase(new Road().cost)(player)(w);
-        return placeRoad(start, end)(player)(purchased);
-    },
-    'Move Thief': (p, {x, y}) => (w) => w, // TODO: Implement rules.
-    'Buy Card': (p) => (w) => w,
-    'Play Card': (p, c) => (w) => w,
-    'Trade': (p1, p2) => (w) => w,
+  BuildHouse: (id, coordinates) => (w) => {
+    if (w.tag === 'Failure') {
+      return w;
+    }
+    const player = findPlayer(id)(w);
+    const purchased = purchase(new House().cost)(player)(w);
+    const placed = placeHouse(coordinates)(player)(purchased);
+    return placed;
+  },
+  BuildCity: (id, coordinates) => (w) => {
+    if (w.tag === 'Failure') {
+      return w;
+    }
+    const player = findPlayer(id)(w);
+    const purchased = purchase(new City().cost)(player)(w);
+    return placeCity(coordinates)(player)(purchased);
+  },
+  BuildRoad: (id, start, end) => (w) => {
+    if (w.tag === 'Failure') {
+      return w;
+    }
+    const player = findPlayer(id)(w);
+    const purchased = purchase(new Road().cost)(player)(w);
+    return placeRoad(start, end)(player)(purchased);
+  },
+  MoveThief: (p, { x, y }) => (w) => w, // TODO: Implement rules.
+  BuyCard: (p) => (w) => w,
+  PlayCard: (p, c) => (w) => w,
+  Trade: (p1, p2) => (w) => w,
 };
 
 // ---- Helpers ----
-export const subtractResources = (resources: Resources, toSubtract: Resources) => {
-    return {
-        clay: resources.clay && toSubtract.clay ? resources.clay - toSubtract.clay : resources.clay,
-        grain: resources.grain && toSubtract.grain ? resources.grain - toSubtract.grain : resources.grain,
-        stone: resources.stone && toSubtract.stone ? resources.stone - toSubtract.stone : resources.stone,
-        wood: resources.wood && toSubtract.wood ? resources.wood - toSubtract.wood : resources.wood,
-        wool: resources.wool && toSubtract.wool ? resources.wool - toSubtract.wool : resources.wool,
-    };
+export const subtractResources = (
+  resources: Resources,
+  toSubtract: Resources,
+) => {
+  return {
+    clay:
+      resources.clay && toSubtract.clay
+        ? resources.clay - toSubtract.clay
+        : resources.clay,
+    grain:
+      resources.grain && toSubtract.grain
+        ? resources.grain - toSubtract.grain
+        : resources.grain,
+    stone:
+      resources.stone && toSubtract.stone
+        ? resources.stone - toSubtract.stone
+        : resources.stone,
+    wood:
+      resources.wood && toSubtract.wood
+        ? resources.wood - toSubtract.wood
+        : resources.wood,
+    wool:
+      resources.wool && toSubtract.wool
+        ? resources.wool - toSubtract.wool
+        : resources.wool,
+  };
 };
 
 export const resourcesAreNonNegative = (resources: Resources) => {
-    return resources.clay && resources.clay >= 0 &&
-        resources.grain && resources.grain >= 0 &&
-        resources.wood && resources.wood >= 0 &&
-        resources.wool && resources.wool >= 0 &&
-        resources.stone && resources.stone >= 0;
+  return (
+    resources.clay &&
+    resources.clay >= 0 &&
+    resources.grain &&
+    resources.grain >= 0 &&
+    resources.wood &&
+    resources.wood >= 0 &&
+    resources.wool &&
+    resources.wool >= 0 &&
+    resources.stone &&
+    resources.stone >= 0
+  );
 };
 
-function fail(reason: string): Failure { return { tag: 'Failure', reason }; }
-function success<T>(t: T): Success<T> { return { tag: 'Success', world: t }; }
+function fail(reason: string): Failure {
+  return { tag: 'Failure', reason };
+}
+function success<T>(t: T): Success<T> {
+  return { tag: 'Success', world: t };
+}
 
-export const purchase = (cost: Resources) => (p: Result<Player>) => (r: Result<World>) => {
-    if (r.tag === 'Failure') { return r; }
-    if (p.tag === 'Failure') { return p; }
-    const resources = subtractResources(p.world.resources, cost);
-    if (!resourcesAreNonNegative(resources)) { return fail(`Player ${p.world.id} cannot afford this.`); }
-    const players = r.world.players.map((pl) => (pl.id === p.world.id) ? {...p.world, resources} : pl);
-    return success({ ...r.world, players });
+export const purchase = (cost: Resources) => (p: Result<Player>) => (
+  r: Result<World>,
+) => {
+  if (r.tag === 'Failure') {
+    return r;
+  }
+  if (p.tag === 'Failure') {
+    return p;
+  }
+  const resources = subtractResources(p.world.resources, cost);
+  if (!resourcesAreNonNegative(resources)) {
+    return fail(`Player ${p.world.id} cannot afford this.`);
+  }
+  const players = r.world.players.map(
+    (pl) => (pl.id === p.world.id ? { ...p.world, resources } : pl),
+  );
+  return success({ ...r.world, players });
 };
 
 const findPlayer = (id: number) => (r: Result<World>) => {
-    if (r.tag === 'Failure') { return r; }
-    const player = r.world.players.find((pl) => pl.id === id);
-    if (!player) { return fail(`Player ${id} not found.`); }
-    return success(player);
+  if (r.tag === 'Failure') {
+    return r;
+  }
+  const player = r.world.players.find((pl) => pl.id === id);
+  if (!player) {
+    return fail(`Player ${id} not found.`);
+  }
+  return success(player);
 };
 
-const placeHouse = (coord: MatrixCoordinate) => (p: Result<Player>) => (r: Result<World>) => {
-    if (r.tag === 'Failure') { return r; }
-    if (p.tag === 'Failure') { return p; }
-    const allHouses = r.world.players.reduce((acc: House[], p) => acc.concat(p.houses), []);
-    const canPlace = allHouses.every((h) =>
-        (h.position.x !== coord.x && h.position.y !== coord.y)
-        && (h.position.y !== coord.y - 1 && h.position.x !== coord.x - 1)  // Adjecent matrix corners.
-        && (h.position.y !== coord.y - 1 && h.position.x !== coord.x + 1)  // Adjecent matrix corners.
-        && (h.position.y !== coord.y + 1 && h.position.x !== coord.x - 1)  // Adjecent matrix corners.
-        && (h.position.y !== coord.y + 1 && h.position.x !== coord.x + 1));
-    if (!canPlace) { return fail('Cannot place a house here!'); }
-    const addedHouse = p.world.houses.concat([new House(coord)]);
-    const players = r.world.players.map((pl) => (pl.id === p.world.id) ? {...p.world, houses: addedHouse} : pl);
-    return success( {...r.world, players });
+const placeHouse = (coord: MatrixCoordinate) => (p: Result<Player>) => (
+  r: Result<World>,
+) => {
+  if (r.tag === 'Failure') {
+    return r;
+  }
+  if (p.tag === 'Failure') {
+    return p;
+  }
+  const allHouses = r.world.players.reduce(
+    (acc: House[], p) => acc.concat(p.houses),
+    [],
+  );
+  const canPlace = allHouses.every(
+    (h) =>
+      h.position.x !== coord.x &&
+      h.position.y !== coord.y &&
+      (h.position.y !== coord.y - 1 && h.position.x !== coord.x - 1) && // Adjecent matrix corners.
+      (h.position.y !== coord.y - 1 && h.position.x !== coord.x + 1) && // Adjecent matrix corners.
+      (h.position.y !== coord.y + 1 && h.position.x !== coord.x - 1) && // Adjecent matrix corners.
+      (h.position.y !== coord.y + 1 && h.position.x !== coord.x + 1),
+  );
+  if (!canPlace) {
+    return fail('Cannot place a house here!');
+  }
+  const addedHouse = p.world.houses.concat([new House(coord)]);
+  const players = r.world.players.map(
+    (pl) => (pl.id === p.world.id ? { ...p.world, houses: addedHouse } : pl),
+  );
+  return success({ ...r.world, players });
 };
 
-const placeCity = (coord: MatrixCoordinate) => (p: Result<Player>) => (r: Result<World>) => {
-    if (r.tag === 'Failure') { return r; }
-    if (p.tag === 'Failure') { return p; }
-    const canPlace = p.world.houses.some((h) => h.position.x === coord.x && h.position.y === coord.y);
-    if (!canPlace) { return fail('Cannot place a house here!'); }
-    const houses = p.world.houses.filter((h) => h.position !== coord);
-    const cities = p.world.cities.concat([new City(coord)]);
-    const players = r.world.players.map((pl) => (pl.id === p.world.id) ? {...p.world, houses, cities} : pl);
-    return success( {...r.world, players });
+const placeCity = (coord: MatrixCoordinate) => (p: Result<Player>) => (
+  r: Result<World>,
+) => {
+  if (r.tag === 'Failure') {
+    return r;
+  }
+  if (p.tag === 'Failure') {
+    return p;
+  }
+  const canPlace = p.world.houses.some(
+    (h) => h.position.x === coord.x && h.position.y === coord.y,
+  );
+  if (!canPlace) {
+    return fail('Cannot place a house here!');
+  }
+  const houses = p.world.houses.filter((h) => h.position !== coord);
+  const cities = p.world.cities.concat([new City(coord)]);
+  const players = r.world.players.map(
+    (pl) => (pl.id === p.world.id ? { ...p.world, houses, cities } : pl),
+  );
+  return success({ ...r.world, players });
 };
 
-const placeRoad = (start: MatrixCoordinate, end: MatrixCoordinate) => (p: Result<Player>) => (r: Result<World>) => {
-    if (r.tag === 'Failure') { return r; }
-    if (p.tag === 'Failure') { return p; }
-    const canPlace = p.world.roads.some((ro) => ro.start !== start && ro.end !== end);
-    if (!canPlace) { return fail('Cannot place a road here!'); }
-    const roads = p.world.roads.concat([new Road(start, end)]);
-    const players = r.world.players.map((pl) => (pl.id === p.world.id) ? {...p.world, roads} : pl);
-    return success( {...r.world, players });
+const placeRoad = (start: MatrixCoordinate, end: MatrixCoordinate) => (
+  p: Result<Player>,
+) => (r: Result<World>) => {
+  if (r.tag === 'Failure') {
+    return r;
+  }
+  if (p.tag === 'Failure') {
+    return p;
+  }
+  const canPlace = p.world.roads.some(
+    (ro) => ro.start !== start && ro.end !== end,
+  );
+  if (!canPlace) {
+    return fail('Cannot place a road here!');
+  }
+  const roads = p.world.roads.concat([new Road(start, end)]);
+  const players = r.world.players.map(
+    (pl) => (pl.id === p.world.id ? { ...p.world, roads } : pl),
+  );
+  return success({ ...r.world, players });
 };
