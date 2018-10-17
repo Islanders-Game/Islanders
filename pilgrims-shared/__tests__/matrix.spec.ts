6ac652f4aebdbb7d4233fb03dbbd3d335f9ba5e7
Anders Wind <awia00@gmail.com>
2018-10-18 00:56:28 +0200

    Some work on matrix to hex - not really working but some tests have been setup

--- pilgrims-shared/__tests__/matrix.spec.ts
+++ pilgrims-shared/__tests__/matrix.spec.ts
@@ -2,7 +2,7 @@ import {
   MatrixCoordinate,
   neighbouringMatrixCoord,
   neighbouringHexCoord,
-} from '../dist/Shared';
+} from '../lib/Shared';
 
 describe('neighbouringMatrixCoord', () => {
   test('Check correct output', () => {
@@ -16,5 +16,43 @@ describe('neighbouringMatrixCoord', () => {
         expected.some(({ x, y }) => element.x == x && element.y == y),
       );
     });
+  });
+});
+
+describe('neighbouringHexCoord', () => {
+  test('Check correct output { x: 3, y: 1 }', () => {
+    const point = { x: 3, y: 1 };
+    const expected = [{ x: 0, y: 0 }, { x: 1, y: 0 }, { x: 1, y: -1 }];
+    const result = neighbouringHexCoord(point);
+    expect(result.length).toEqual(expected.length);
+    expect(result).toEqual(expect.arrayContaining(expected));
+  });
+  test('Check correct output { x: 2, y: 2 }', () => {
+    const point = { x: 2, y: 2 };
+    const expected = [{ x: 0, y: 0 }, { x: 1, y: 0 }, { x: 0, y: 1 }];
+    const result = neighbouringHexCoord(point);
+    expect(result.length).toEqual(expected.length);
+    expect(result).toEqual(expect.arrayContaining(expected));
+  });
+  test('Check correct output { x: 1, y: 2 }', () => {
+    const point = { x: 1, y: 2 };
+    const expected = [{ x: 0, y: 0 }, { x: -1, y: 0 }, { x: 0, y: 1 }];
+    const result = neighbouringHexCoord(point);
+    expect(result.length).toEqual(expected.length);
+    expect(result).toEqual(expect.arrayContaining(expected));
+  });
+  test('Check correct output { x: 4, y: 1 }', () => {
+    const point = { x: 4, y: 1 };
+    const expected = [{ x: 1, y: -1 }, { x: 1, y: 0 }, { x: 2, y: 0 }];
+    const result = neighbouringHexCoord(point);
+    expect(result.length).toEqual(expected.length);
+    expect(result).toEqual(expect.arrayContaining(expected));
+  });
+  test('Check correct output { x: 5, y: 2 }', () => {
+    const point = { x: 5, y: 2 };
+    const expected = [{ x: 1, y: 1 }, { x: 1, y: 0 }, { x: 2, y: 0 }];
+    const result = neighbouringHexCoord(point);
+    expect(result.length).toEqual(expected.length);
+    expect(result).toEqual(expect.arrayContaining(expected));
   });
 });
--- pilgrims-shared/lib/MatrixCoordinate.ts
+++ pilgrims-shared/lib/MatrixCoordinate.ts
@@ -35,5 +35,31 @@ export const neighbouringHexCoord = (
   coord: MatrixCoordinate,
 ): HexCoordinate[] => {
   const result: HexCoordinate[] = [];
+  const hexX = Math.floor(coord.x / 3);
+  const hexY = Math.floor(coord.y / 2.0);
+  if (coord.y % 2 == 0) {
+    // add hexY and hexY-1
+    if (coord.x % 4 == 1) {
+      result.push({ x: hexX, y: hexY - 1 });
+      result.push({ x: hexX - 1, y: hexY - 1 });
+      result.push({ x: hexX, y: hexY });
+    } else if (coord.x % 4 == 2) {
+      result.push({ x: hexX, y: hexY });
+      result.push({ x: hexX, y: hexY - 1 });
+      result.push({ x: hexX + 1, y: hexY - 1 });
+    }
+  } else {
+    // add hexY and hexY +1
+    if (coord.x % 4 == 0) {
+      result.push({ x: hexX, y: hexY - 1 });
+      result.push({ x: hexX, y: hexY });
+      result.push({ x: hexX + 1, y: hexY });
+    }
+    if (coord.x % 4 == 3) {
+      result.push({ x: hexX - 1, y: hexY });
+      result.push({ x: hexX, y: hexY });
+      result.push({ x: hexX, y: hexY - 1 });
+    }
+  }
   return result;
 };
