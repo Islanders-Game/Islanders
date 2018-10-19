984bd68f94513a6a90d303e17ce4bcfae4f08af0
Anders Wind <awia00@gmail.com>
2018-10-19 10:30:11 +0200

    Improved the tests of matrixCoord

--- pilgrims-shared/__tests__/matrix.spec.ts
+++ pilgrims-shared/__tests__/matrix.spec.ts
@@ -5,52 +5,26 @@ import {
 } from '../lib/Shared';
 
 describe('neighbouringMatrixCoord', () => {
-  test('Check correct output', () => {
-    const point = { x: 1, y: 0 };
-    const expected = [{ x: 0, y: 1 }, { x: 2, y: 0 }, { x: 0, y: -1 }];
+  test.each([
+    [{ x: 1, y: 0 }, [{ x: 0, y: 1 }, { x: 0, y: -1 }, { x: 2, y: 0 }]],
+    [{ x: 4, y: 1 }, [{ x: 3, y: 1 }, { x: 5, y: 0 }, { x: 5, y: 2 }]],
+    [{ x: 3, y: 3 }, [{ x: 2, y: 2 }, { x: 2, y: 4 }, { x: 4, y: 3 }]],
+  ])('Check correct output', (point, expected) => {
     const result = neighbouringMatrixCoord(point);
     expect(result.length).toEqual(expected.length);
-
-    result.forEach((element) => {
-      expect(true).toEqual(
-        expected.some(({ x, y }) => element.x === x && element.y === y),
-      );
-    });
+    expect(result).toEqual(expect.arrayContaining(expected));
   });
 });
 
 describe('neighbouringHexCoord', () => {
-  test('Check correct output { x: 3, y: 1 }', () => {
-    const point = { x: 3, y: 1 };
-    const expected = [{ x: 0, y: 0 }, { x: 1, y: 0 }, { x: 1, y: -1 }];
-    const result = neighbouringHexCoord(point);
-    expect(result.length).toEqual(expected.length);
-    expect(result).toEqual(expect.arrayContaining(expected));
-  });
-  test('Check correct output { x: 2, y: 2 }', () => {
-    const point = { x: 2, y: 2 };
-    const expected = [{ x: 0, y: 0 }, { x: 1, y: 0 }, { x: 0, y: 1 }];
-    const result = neighbouringHexCoord(point);
-    expect(result.length).toEqual(expected.length);
-    expect(result).toEqual(expect.arrayContaining(expected));
-  });
-  test('Check correct output { x: 1, y: 2 }', () => {
-    const point = { x: 1, y: 2 };
-    const expected = [{ x: 0, y: 0 }, { x: -1, y: 0 }, { x: 0, y: 1 }];
-    const result = neighbouringHexCoord(point);
-    expect(result.length).toEqual(expected.length);
-    expect(result).toEqual(expect.arrayContaining(expected));
-  });
-  test('Check correct output { x: 4, y: 1 }', () => {
-    const point = { x: 4, y: 1 };
-    const expected = [{ x: 1, y: -1 }, { x: 1, y: 0 }, { x: 2, y: 0 }];
-    const result = neighbouringHexCoord(point);
-    expect(result.length).toEqual(expected.length);
-    expect(result).toEqual(expect.arrayContaining(expected));
-  });
-  test('Check correct output { x: 5, y: 2 }', () => {
-    const point = { x: 5, y: 2 };
-    const expected = [{ x: 1, y: 0 }, { x: 2, y: 0 }, { x: 2, y: 1 }];
+  test.each([
+    [{ x: 3, y: 1 }, [{ x: 0, y: 0 }, { x: 1, y: 0 }, { x: 1, y: -1 }]],
+    [{ x: 2, y: 2 }, [{ x: 0, y: 0 }, { x: 1, y: 0 }, { x: 0, y: 1 }]],
+    [{ x: 1, y: 2 }, [{ x: 0, y: 0 }, { x: -1, y: 0 }, { x: 0, y: 1 }]],
+    [{ x: 4, y: 1 }, [{ x: 1, y: -1 }, { x: 1, y: 0 }, { x: 2, y: 0 }]],
+    [{ x: 5, y: 2 }, [{ x: 1, y: 0 }, { x: 2, y: 0 }, { x: 2, y: 1 }]],
+  ])
+  ('Check correct output { x: 3, y: 1 }', (point, expected) => {
     const result = neighbouringHexCoord(point);
     expect(result.length).toEqual(expected.length);
     expect(result).toEqual(expect.arrayContaining(expected));
