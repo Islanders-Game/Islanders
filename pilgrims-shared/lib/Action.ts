bd6a0c3ec597b12d019fd3e7921cd426ae5caad6
Anders Wind <awia00@gmail.com>
2018-10-20 21:10:26 +0200

    Started working on actions

--- pilgrims-shared/lib/Action.ts
+++ pilgrims-shared/lib/Action.ts
@@ -2,17 +2,62 @@ import { MatrixCoordinate } from './MatrixCoordinate';
 import { HexCoordinate } from './HexCoordinate';
 import { DevelopmentCard } from './Entities/DevelopmentCard';
 import { Resources } from './Resources';
+import { Hex } from 'honeycomb-grid';
 
+// An action is an closure which has the information neccesary to perform one rule on a world.
+
+export type ActionString =
+  | 'BuildHouse'
+  | 'BuildCity'
+  | 'BuildRoad'
+  | 'Thief'
+  | '';
 export interface Action {
-  buildHouse: undefined | { playerID: string; coordinates: MatrixCoordinate };
-  buildCity: undefined | { playerID: string; coordinates: MatrixCoordinate };
-  buildRoad:
-    | undefined
-    | { playerID: string; start: MatrixCoordinate; end: MatrixCoordinate };
-  moveThief: undefined | { playerID: string; coordinates: HexCoordinate };
-  buyCard: undefined | { playerID: string };
-  playCard: undefined | { playerID: string; card: DevelopmentCard };
-  trade:
-    | undefined
-    | { playerID: string; otherPlayerID: string; resources: Resources };
+  type: ActionString;
+  parameters: any;
+}
+
+type BuildHouseParameters = { playerID: string; coordinates: MatrixCoordinate };
+type BuildCityParameters = { playerID: string; coordinates: MatrixCoordinate };
+type BuildRoadParameters = {
+  playerID: string;
+  start: MatrixCoordinate;
+  end: MatrixCoordinate;
+};
+type PlaceThiefParameters = { playerID: string; coord: HexCoordinate };
+
+export class BuildHouseAction implements Action {
+  public type: ActionString = 'BuildHouse';
+  public parameters: BuildHouseParameters;
+  constructor(playerID: string, coordinates: MatrixCoordinate) {
+    this.parameters = { playerID: playerID, coordinates: coordinates };
+  }
+}
+
+export class BuildCityAction implements Action {
+  public type: ActionString = 'BuildCity';
+  public parameters: BuildCityParameters;
+  constructor(
+    playerID: string,
+    start: MatrixCoordinate,
+    end: MatrixCoordinate,
+  ) {
+    this.parameters = { playerID: playerID, start: start, end: end };
+  }
+}
+
+export class BuildRoadAction implements Action {
+  public type: ActionString = 'BuildRoad';
+  public parameters: BuildRoadAction;
+  constructor(playerID: string, coordinates: MatrixCoordinate) {
+    this.parameters = { playerID: playerID, coordinates: coordinates };
+  }
+}
+
+export class ThiefAction implements Action {
+  public type: ActionString = 'Thief';
+  public parameters: PlaceThiefParameters;
+  constructor(playerID: string, coordinates: HexCoordinate) {
+    this.parameters = { playerID: playerID, coordinates: coordinates };
+  }
 }
