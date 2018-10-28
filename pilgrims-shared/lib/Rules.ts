b164ef8c5d22e82de2366893dcc7faca06166776
Anders Wind <awia00@gmail.com>
2018-10-28 23:13:46 +0100

    Build road cannot be "doubled" placed anymore

--- pilgrims-shared/lib/Rules.ts
+++ pilgrims-shared/lib/Rules.ts
@@ -281,7 +281,15 @@ const placeRoad = (start: MatrixCoordinate, end: MatrixCoordinate) => (
   const player = r.value.players.find((pl) => pl.name === playerName)!;
 
   const canPlace = !player.roads.some(
-    (ro) => ro.start === start && ro.end === end,
+    (ro) =>
+      (ro.start.x === start.x &&
+        ro.start.y === start.y &&
+        ro.end.x === end.x &&
+        ro.end.y === end.y) ||
+      (ro.start.x === end.x &&
+        ro.start.y === end.y &&
+        ro.end.x === start.x &&
+        ro.end.y === start.y),
   );
   if (!canPlace) {
     return fail(`Can't place a road here!`);
