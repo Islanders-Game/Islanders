9e5526772bf7aaaa3083f0f6f548d16dcefc9bfd
Anders Fischer <andersfischern@me.com>
2018-11-20 15:20:32 +0100

    Added dummy dialog for selecting a development card in UI.

---
+++ pilgrims-client/src/components/DevelopmentCardSelect.vue
@@ -0,0 +1,32 @@
+<template>
+    <v-dialog
+      v-model="dialog"
+      width="800"
+    >
+      <v-card>
+        <v-card-title
+          class="headline"
+          primary-title
+        >
+        Development Card
+        </v-card-title>
+
+        <v-card-text>
+          This is a development card. 
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="primary"
+            flat
+            @click="dialog = false"
+          >
+            Select card
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+</template>
