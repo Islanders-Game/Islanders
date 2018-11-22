a3f415e2e53d76721a374a9530982d0cf3815c13
Anders Fischer <andersfischern@me.com>
2018-11-22 13:30:23 +0100

    Fixed layout.

--- pilgrims-client/src/components/DevelopmentCardSelect.vue
+++ pilgrims-client/src/components/DevelopmentCardSelect.vue
@@ -1,32 +1,51 @@
 <template>
-    <v-dialog
-      v-model="dialog"
-      width="800"
-    >
-      <v-card>
-        <v-card-title
-          class="headline"
-          primary-title
-        >
-        Development Card
-        </v-card-title>
+    <div class="text-xs-center">
+      <v-dialog
+        v-model="dialog"
+        width="500"
+      >
+        <v-layout>
+          <v-flex xs12>
+            <v-card>
+              <v-container
+                fluid
+                grid-list-md
+              >
+                <v-layout row wrap>
+                  <v-flex xs6
+                    v-for="card in cards"
+                    :key="card.type"
+                  >
+                    <v-card>
+                        <v-container
+                          fill-height
+                          fluid
+                          pa-2
+                        >
+                          <v-layout fill-height>
+                            <v-flex xs12 align-end flexbox>
+                              <span class="headline" v-text="card.type"></span>
+                            </v-flex>
+                          </v-layout>
+                        </v-container>
 
-        <v-card-text>
-          This is a development card. 
-        </v-card-text>
-
-        <v-divider></v-divider>
-
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn
-            color="primary"
-            flat
-            @click="dialog = false"
-          >
-            Select card
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
+                      <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn
+                          color="primary"
+                          flat
+                          @click="dialog = false"
+                        >
+                          Select
+                        </v-btn>
+                      </v-card-actions>
+                    </v-card>
+                  </v-flex>
+                </v-layout>
+              </v-container>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-dialog>
+    </div>
 </template>
