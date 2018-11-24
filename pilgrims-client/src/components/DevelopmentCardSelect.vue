794f0f1234767439f5d30090c01beba6858afb0d
Anders Fischer <andersfischern@me.com>
2018-11-24 16:58:10 +0100

    Fixed type rendering.

--- pilgrims-client/src/components/DevelopmentCardSelect.vue
+++ pilgrims-client/src/components/DevelopmentCardSelect.vue
@@ -25,7 +25,7 @@
                         >
                           <v-layout fill-height>
                             <v-flex xs12 align-end flexbox>
-                              <span class="headline">{{card}}</span>
+                              <span class="headline">{{card.type}}</span>
                             </v-flex>
                           </v-layout>
                         </v-container>
