b275ed8e9825cf14495f92cd3cac5c886ba0d024
Anders Wind <awia00@gmail.com>
2019-03-26 23:20:26 +0100

    Fixed typescript bugs for jest (arguments for test.each must either have same type or be casted)

--- pilgrims-shared/__tests__/matrix.spec.ts
+++ pilgrims-shared/__tests__/matrix.spec.ts
@@ -11,9 +11,9 @@ describe('neighbouringMatrixCoords', () => {
     [{ x: 4, y: 1 }, [{ x: 3, y: 1 }, { x: 5, y: 0 }, { x: 5, y: 2 }]],
     [{ x: 3, y: 3 }, [{ x: 2, y: 2 }, { x: 2, y: 4 }, { x: 4, y: 3 }]],
   ])('Check correct output', (point, expected) => {
-    const result = neighbouringMatrixCoords(point);
-    expect(result.length).toEqual(expected.length);
-    expect(result).toEqual(expect.arrayContaining(expected));
+    const result = neighbouringMatrixCoords(<MatrixCoordinate>point);
+    expect(result.length).toEqual((<Array<MatrixCoordinate>>expected).length);
+    expect(result).toEqual(expect.arrayContaining((<Array<MatrixCoordinate>>expected)));
   });
 });
 
@@ -25,9 +25,9 @@ describe('neighbouringHexCoord', () => {
     [{ x: 4, y: 1 }, [{ x: 1, y: -1 }, { x: 1, y: 0 }, { x: 2, y: 0 }]],
     [{ x: 5, y: 2 }, [{ x: 1, y: 0 }, { x: 2, y: 0 }, { x: 2, y: 1 }]],
   ])('Check correct output', (point, expected) => {
-    const result = neighbouringHexCoords(point);
-    expect(result.length).toEqual(expected.length);
-    expect(result).toEqual(expect.arrayContaining(expected));
+    const result = neighbouringHexCoords(<MatrixCoordinate>point);
+    expect(result.length).toEqual((<Array<MatrixCoordinate>>expected).length);
+    expect(result).toEqual(expect.arrayContaining((<Array<MatrixCoordinate>>expected)));
   });
 });
 
@@ -41,7 +41,7 @@ describe('getMatrixCoordCorner zeroCase', () => {
     [{ x: 1, y: 2 }, 3, { x: 2, y: 6 }],
     [{ x: 2, y: 2 }, 3, { x: 4, y: 5 }],
   ])('Check correct output', (point, cornerIndex, expected) => {
-    const result = getMatrixCoordCorner(point, cornerIndex);
-    expect(result).toEqual(expected);
+    const result = getMatrixCoordCorner(<MatrixCoordinate>point, <number>cornerIndex);
+    expect(result).toEqual(<MatrixCoordinate>expected);
   });
 });
