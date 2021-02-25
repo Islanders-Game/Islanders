5a9a9ec3a5d70f612e52dadc3899d80838cb3d2c
Anders Wind <awia00@gmail.com>
2021-02-25 20:01:07 +0100

    Added utility functions in tile and hex to more easily change between them

--- colonists-shared/lib/Tile.ts
+++ colonists-shared/lib/Tile.ts
@@ -1,4 +1,4 @@
-import { HexCoordinate } from './HexCoordinate';
+import { HexCoordinate, hexEquals } from './HexCoordinate';
 
 export type HarborType =
   | 'WoodHarbor'
@@ -7,30 +7,14 @@ export type HarborType =
   | 'ClayHarbor'
   | 'StoneHarbor'
   | 'ThreeToOneHarbor';
-export type TileType =
-  | 'Wood'
-  | 'Stone'
-  | 'Clay'
-  | 'Grain'
-  | 'Wool'
-  | 'Desert'
-  | 'Ocean'
-  | HarborType;
-export type DiceRoll =
-  | 'None'
-  | 2
-  | 3
-  | 4
-  | 5
-  | 6
-  | 7
-  | 8
-  | 9
-  | 10
-  | 11
-  | 12;
+export type TileType = 'Wood' | 'Stone' | 'Clay' | 'Grain' | 'Wool' | 'Desert' | 'Ocean' | HarborType;
+export type DiceRoll = 'None' | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
 export interface Tile {
   type: TileType;
   diceRoll: DiceRoll;
   coord: HexCoordinate;
+}
+
+export function findTileInMap(map: Tile[], hex: HexCoordinate): Tile | undefined {
+  return map.find((x) => hexEquals(x.coord, hex));
 }
